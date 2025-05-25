4760d0e1d221d9a719125b4a1c5694799c656963
Raghupremshahapuram <100329614+Raghupremshahapuram@users.noreply.github.com>
2025-05-25 09:00:18 +0530

    updated server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const port = 6700;
+const port = process.env.PORT || 6700;
 const fs = require('fs');
 const path = require('path');
 
